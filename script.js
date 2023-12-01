f3c6f383bc32bb240efc6bd66329061c601660a6
DenisEnache <149329298+DenisEnache@users.noreply.github.com>
2023-12-01 22:52:23 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var wordsList = ["MAJOR", "TIME", "PATIENT", "KINDNESS", "DOCTOR", "LIFE", "SLENDERING", 
+"IMPERSONATION", "ENCOMPASS", "BUTTER", "FOOTBALL", "SIMPLICITY", "MISTERY", "NEGOCIABLE"];
+
+const alphabet = document.getElementById("alphabet");
+const wordContainer = document.getElementById("word-container");
+const gameStatus = document.getElementById("gameStatus");
+
+let randomPos = Math.floor(Math.random() * wordsList.length);
+let randomWord  = wordsList[randomPos];
+let lifeNr = 7;
+let guessedLetNr = 0;
+
+function wordLines() {
+    for (let i = 0; i < randomWord.length; ++i) {
+        let line = document.createElement("span");
+        line.textContent = " ";
+        line.classList.add("line"); 
+        wordContainer.appendChild(line);
+    }
+}
+
+function replaceLine(index, letter) {
+    let lines = document.querySelectorAll(".line");
+    for (let i = 0; i < lines.length; ++i) {
+        if (index < lines.length) {
+            lines[index].textContent = letter;
+            lines[index].classList.add("white-line"); 
+        }
+    }
+}
+
+function letters() {
+    for(let i = 'A'.charCodeAt(0); i <= 'Z'.charCodeAt(0); ++i) {
+        const letterButton = document.createElement("button");
+        letterButton.textContent = String.fromCharCode(i);
+        letterButton.classList.add("btn-primary", "btn-lg", "p-3", "m-3");
+        letterButton.addEventListener("click", function() {
+            let life = false;
+            for (let j = 0; j < randomWord.length && lifeNr > 0; ++j) {
+                if (String.fromCharCode(i) == randomWord[j]) {
+                    replaceLine(j, String.fromCharCode(i));
+                    life = true;
+                    ++guessedLetNr;
+                }
+            }
+            if (life == false && lifeNr > 0 && guessedLetNr < randomWord.length) {
+                --lifeNr;
+                document.getElementById("lifeCount").textContent = lifeNr;
+            }
+            gameStatus.classList.add("game-status");
+            if (lifeNr == 0) {
+                gameStatus.textContent = "GAME OVER";
+            } else if (guessedLetNr == randomWord.length) {
+                gameStatus.textContent = "CONGRATULATIONS!";
+            }
+        });
+        alphabet.appendChild(letterButton);
+    }
+}
+
+letters();
+wordLines();
